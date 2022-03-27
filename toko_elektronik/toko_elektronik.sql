8a13c28c3475f80a24d7efd40ac1f22c6a3054f9
bagasputra2660 <54340291+bagasputra2660@users.noreply.github.com>
2022-03-28 00:22:06 +0700

    Add files via upload

---
+++ toko_elektronik/toko_elektronik.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Mar 2022 pada 19.12
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_elektronik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga_produk` varchar(100) NOT NULL,
+  `foto_produk` text DEFAULT NULL,
+  `stok_produk` varchar(100) NOT NULL,
+  `deskripsi_produk` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga_produk`, `foto_produk`, `stok_produk`, `deskripsi_produk`) VALUES
+(5, 'canon printers ', '15000000', '03ba214b33de44ac898705653839aa30_G3x60_AS_FR_cl_nvy_en-362x320.png', '8', 'printers canon'),
+(6, 'epson printers  ', '20000000', 'WhatsApp_Image_2022-03-24_at_09.09.00-removebg-preview.png', '6', 'epson'),
+(7, 'canon printers ', '19000000', '7571fdd034eb48b0afa5395d12a285cb_GM40XX_AS_FR_cl_en-362.png', '10', 'canon printers'),
+(8, 'hp printers', '17000000', '61cd12329f200-removebg-preview.png', '18', 'hp printers\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_hp` varchar(13) NOT NULL,
+  `nama_produk` varchar(100) NOT NULL,
+  `harga_produk` varchar(100) NOT NULL,
+  `subtotal` varchar(100) NOT NULL,
+  `foto_produk` text DEFAULT NULL,
+  `status` enum('proses','dikirim','ditolak') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `roles` enum('Admin','Customer') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `name`, `password`, `roles`) VALUES
+(7, 'admin', 'admin', 'admin', 'Admin'),
+(9, 'bagas', 'bagas', 'bagas123', 'Customer'),
+(10, 'blodeaters', 'blodeaters', '12345', 'Customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
